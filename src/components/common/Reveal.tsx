6ff98e7332a2dc17bacd1250329175c244f6572f
Rushi Chudasama <rushi.chudasama@visilean.com>
2024-07-30 12:59:04 +0530

    added reveal effect for home page title

---
+++ src/components/common/Reveal.tsx
@@ -0,0 +1,69 @@
+import { motion, useAnimation, useInView } from "framer-motion";
+import { useEffect, useRef } from "react";
+
+interface Props {
+  children: JSX.Element;
+  width?: "fit-content" | "full-width";
+}
+
+export const Reveal = ({ children, width = "fit-content" }: Props) => {
+  const revealRef = useRef<HTMLDivElement>(null);
+  const isInView = useInView(revealRef, { once: true });
+
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      slideControls.start("visible");
+    } else {
+      mainControls.start("hidden");
+      slideControls.start("hidden");
+    }
+  }, [isInView]);
+  return (
+    <div
+      ref={revealRef}
+      style={{ position: "relative", width, overflow: "hidden" }}
+    >
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 100 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        exit="hidden"
+        transition={{
+          duration: 0.5,
+          delay: 0.25,
+        }}
+      >
+        {children}
+      </motion.div>
+      <motion.div
+        className="bg-secondary"
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        exit="hidden"
+        transition={{
+          duration: 0.35,
+          ease: "easeIn",
+        }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          zIndex: 20,
+        }}
+      />
+    </div>
+  );
+};
